3c8dce92e01c4b0089cac7a20e6b9fdb643c8bb2
pushan <pushanalagiya@gmail.com>
2024-02-06 14:53:13 +0530

    first commit

---
+++ src/js/config.js
@@ -0,0 +1,8 @@
+// Configuration file that contains all global variables that defines important Data
+
+export const API_URL = "https://forkify-api.herokuapp.com/api/v2/recipes/";
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = "94f5c03a-5891-45a2-a17a-45021cc688fe";
+// Time to Automatically-close the form window
+export const MODAL_CLOSE_SEC = 2;
